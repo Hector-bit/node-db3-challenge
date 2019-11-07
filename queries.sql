b6ac76262da8e37d799d333c62ec70f05c8402d1
Hector-bit <hectormartinez.2978@gmail.com>
2019-11-07 02:16:52 -0800

    quires are done

--- queries.sql
+++ queries.sql
@@ -2,8 +2,28 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+select Product.ProductName, Category.CategoryName 
+from [Category]
+join Product on Product.CategoryId = Category.Id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+select [Order].Id, Shipper.CompanyName 
+from [Shipper]
+join [Order] on [Order].ShipVia = Shipper.Id
+where OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+select OrderDetail.*, Product.ProductName 
+from OrderDetail
+join Product
+on Product.Id = OrderDetail.ProductId
+where OrderDetail.OrderId = '10251';
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+select [Order].Id, [Customer].CompanyName, [Employee].LastName
+from [Order]
+join [Customer], [Employee]
+where [Order].CustomerId = [Customer].Id AND [Order].EmployeeId = [Employee].Id
